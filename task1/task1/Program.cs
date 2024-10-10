3927daca5ac7c524bad3a390d7a058c49470d98d
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-10 13:02:54 +0400

    initial

---
+++ task1/task1/Program.cs
@@ -0,0 +1,1 @@
+﻿namespace task1_
---
+++ task2/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());    
+            int c = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void Area(int r)
+        {
+            int p = 3;
+
+            Console.WriteLine("dairenin sahesi " + p * r * r);
+        }
+        public static void Area(int a, int b)
+        {
+            Console.WriteLine("duzbucaqlinin sahesi " + a * b);
+        }
+        public static void Area(int a, int b, int c)
+        {
+            Console.WriteLine("duzbucaqli paralelepipedin tam sethinin sahesi " + 2 * (a * b + a * c + b * c));
+        }
+        public static void Area(double a, double b, double c)
+        {
+            Console.WriteLine("p = " + (a + b + c) / 2);
+        }
+    }
+}
