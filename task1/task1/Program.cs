700174beab5b9ece487d1d0c5fc3dee32f2cab8f
crispyyy-365 <huseynaa-bp217@code.edu.az>
2024-10-10 18:07:36 +0400

    Update task1

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -1,1 +1,54 @@
-﻿namespace task1_
+//bu kodu men evvelceden yazmisdim amma yuklendiyi qovlugun icindeki basqa bir qovluga salanda proqram itdi ve VS-de acmaqa calisdim acmirdi silinmis oldugunu deyirdi. 
+
+//basa dusurem gec yuklendiyine gore qiymetlendirilmeyecek amma yene de bu koda baxardiz zehmet olmasa.
+using System;
+
+namespace task1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // burda arrayi yaradan metod duzeltmek istedim amma vaxtim yox idi onsuz da esas mesele bu deyil.
+            int[] num = { };
+
+            Console.WriteLine("Length of the array : ");
+
+            int length = Convert.ToInt32(Console.ReadLine());
+            Array.Resize(ref num, length);
+
+            Console.WriteLine("numbers : ");
+
+            for (int i = 0; i < length; i++)
+            {
+                int element = Convert.ToInt32(Console.ReadLine());
+
+                num[i] = element;
+            }
+            GetMin(num);
+        }
+        public static void GetMin(int[] num)
+        {
+            int count = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = 0; j < num.Length; j++)
+                {
+                    if (num[i] > num[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > 0)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    Console.WriteLine("min number : " + num[i]);
+                }
+            }
+        }
+    }
+}
